0687cec9f40920fddc7749d5719d3d40edd3e2a5
Yeltsin Anwar Reyes Blandón <yeltsin.reyes95@gmail.com>
2024-09-06 11:34:45 -0600

    feat: Agregar modelo de usuario y campos requeridos en el esquema de Prisma
    Se agregó el modelo de usuario en el esquema de Prisma, incluyendo los campos requeridos como nombre, apellido y contraseña. Además, se eliminó el campo saltedHashedPassword y se agregó el campo password en la tabla de usuarios.
    
    Se creó un controlador y una ruta para registrar usuarios, validando los campos requeridos y la validez del correo electrónico y la contraseña.
    
    Se agregó un servicio para crear usuarios en la base de datos utilizando Prisma.
    
    Se agregaron validadores para verificar la validez del correo electrónico y la contraseña.

---
+++ src/modules/users/usersController.js
@@ -0,0 +1,24 @@
+const usersService = require("./usersService");
+const { validateEmail, validatePassword } = require("../../utils/validators");
+
+const registerUser = async (req, res) => {
+  const { email, name, lastname, password } = req.body;
+  if (!email || !name || !lastname || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  if (!validatePassword(password)) {
+    return res
+      .status(400)
+      .json({
+        message:
+          "Password must be 8 characters long, and must contain at least one lowercase character, one uppercase character, and at least one number.",
+      });
+  }
+  if (!validateEmail(email)) {
+    return res.status(400).json({ message: "Invalid email" });
+  }
+  const result = await usersService.createUser(email, name, lastname, password);
+  res.status(result.status).json(result.message);
+};
+
+module.exports = registerUser;
---
+++ src/utils/validators.js
@@ -0,0 +1,17 @@
+function validateEmail(email) {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+  let flag = 0;
+  if (password.length < 8) flag++;
+  if (!password.match(/[a-z]/)) flag++;
+  if (!password.match(/[A-Z]/)) flag++;
+  if (!password.match(/[0-9]/)) flag++;
+
+  if (flag > 0) return false;
+  else return true;
+}
+
+module.exports = { validateEmail, validatePassword };
